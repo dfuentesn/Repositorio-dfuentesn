fee64978796923f308b1503a07dcf3b59107baf3
dfuentesn <dfuentesn@unal.edu.co>
2025-04-09 08:54:40 -0500

    juego de adivinanza ultima actualizacion

---
+++ 09-04-2025 programacion/guessed-game-FOR.cpp
@@ -0,0 +1,27 @@
+/*
+ Juego de adivinar un numero
+ Pregunta al usuario el numero
+ Informar si lo adivino o no*/
+
+
+#include <iostream>
+
+int main(void) {
+    const int NUM = 10;
+    int guessed_number = NUM/2;
+
+    while(guessed_number != NUM) {
+    std::cout << "Adivina un numero entre 1 y 100:\n";
+    std::cin >> guessed_number;
+
+    if (guessed_number == NUM){
+        std::cout <<"GANASTE :D!!!!\n";
+    } else if (guessed_number > NUM) {
+        std::cout <<"Te pasaste :(. Vuelve a intentar\n";
+    }  else { // es menor
+            std::cout <<"Te falta. Vuele a intentar :)\n"; 
+    }
+    }
+
+    return 0;
+}
