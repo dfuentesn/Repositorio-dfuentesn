482b6296255a991218ba7bc842c99fffd0acf84f
dfuentesn <dfuentesn@unal.edu.co>
2025-04-22 21:19:30 -0500

    Ultima correccion del codigo

--- workshops/Aproximacion-del-numero-pi.cpp
+++ workshops/Aproximacion-del-numero-pi.cpp
@@ -1,44 +1,45 @@
-//Version FINAL del codigo para la aproximacion de pi con la formula de Bailey-Borwein-Plouffe
+// Version FINAL del codigo para la aproximacion de pi con la formula de Bailey-Borwein-Plouffe
 
 #include <iostream>
-#include <cmath> 
+#include <cmath>
 
-//Vuelvo a definir M_PI en caso de que la libreria nos retorne un error de portabilidad
-#ifndef M_PI 
+// Vuelvo a definir M_PI en caso de que la libreria nos retorne un error de portabilidad
+#ifndef M_PI
 const double M_PI = 3.14159265358979323846;
 #endif
 
-//Luego, creo la funcion pi_aprox con double para poder almacenar decimales (a diferencia de int) con varios digitos (a diferencia de float)
-double pi_aprox(int n){
+// Luego, creo la funcion pi_aprox con double para poder almacenar decimales (a diferencia de int) con varios digitos (a diferencia de float)
+double pi_aprox(int n)
+{
     double suma = 0.0;
-    
-    for (int k = 0; k < n; k++) //En este caso uso solo < para retornar exactamente n terminos
-    { 
-        double termino = (4.0/(8*k+1)) - 
-                         (2.0/(8*k+4)) - 
-                         (1.0/(8*k+5)) - 
-                         (1.0/(8*k+6));
-        //En el termino separe el denominador de las restas para una mayor presicion
 
-        termino /= std::pow(16, k); //En esta linea divido el termino por 16**k
-        suma += termino; 
+    for (int k = 0; k < n; k++) // En este caso uso solo < para retornar exactamente n terminos
+    {
+        double termino = (4.0 / (8 * k + 1)) -
+                         (2.0 / (8 * k + 4)) -
+                         (1.0 / (8 * k + 5)) -
+                         (1.0 / (8 * k + 6));
+        // En el termino separe el denominador de las restas para una mayor presicion
+
+        termino /= std::pow(16, k); // En esta linea divido el termino por 16**k
+        suma += termino;
     }
-    
-    return suma; //Aca retorno las sumatorias, eso nos dara la aproximacion de pi
+
+    return suma; // Aca retorno las sumatorias, eso nos dara la aproximacion de pi
 }
 
-int main(){
+int main()
+{
     std::cout.precision(16);
-    std::cout.setf(std::ios::scientific);
+    std::cout.setf(std::ios::scientific); // Configuracion proporcionada para la notacion cientifica
 
-    for (int n = 1; n <= 20; n++) //Aqui defino la variacion del loop entre 1 y 20
+    for (int n = 1; n <= 20; n++) // Aqui defino la variacion del loop entre 1 y 20
     {
-    double diff_rel = std::abs(1 - pi_aprox(n) /M_PI);
+        double dif = std::abs(1 - pi_aprox(n) / M_PI);
 
-    std::cout << "n = ";
-    if (n < 10) std::cout << " ";
-    std::cout << n << ", π ≈ " << pi_aprox(n)
-    << ", Diferencia relativa: " << diff_rel << std::endl;
-     }
-     return 0;
-  }
+        std::cout << "\n" << "π ≈ " << pi_aprox(n) << "\n";
+        std::cout << "n = " << n << "\n";
+        std::cout << "Diferencia relativa = " << dif << "\n";
+    }
+    return 0;
+}
