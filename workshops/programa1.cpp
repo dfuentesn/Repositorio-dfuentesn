d62f110a9ea73c8c37625ac436f585ffc2f3e44a
dfuentesn <dfuentesn@unal.edu.co>
2025-04-29 17:27:51 -0500

    version final del programa1

--- workshops/programa1.cpp
+++ workshops/programa1.cpp
@@ -1,48 +1,58 @@
 #include <iostream>
 #include <cmath>
+#include <string>
 
-bool isprime(long n)
-{
+//establecemos la funcion isprime para definit que es un primo
+bool isprime(long n){
 
-    if (n <= 1)
-    {
-        std::cerr << "Out of range number: " << n << "\n";
+    // aca establecemos un limite inferior, todo n igual o menor a 1 no sera ni tendra primmos por ende su suma sera 0 
+    if (n <= 1){
         return false;
-    }
+    } 
 
+    // aca decimos que 2 es el unico par primo porque retorna como verdadero
     bool flag = true;
-    if (n == 2)
-    {
+    if (n == 2){
         return true;
-    }
+    } 
 
-    for (long ii = 2; ii <= std::sqrt(n) + 1; ii++)
-    {
-        if (n % ii == 0)
-        {
+    //iteramos desde 2 y verificamos que todos los demas n sean divisibles por la raiz del numero mas 1 (el 1 es un precaucion, como vizto en clase)
+    for (long ii = 2; ii <= std::sqrt(n) + 1; ii++){
+        if (n % ii == 0){
             flag = false;
             break;
-        }
+        } 
     }
     return flag;
 }
 
-int main(int argc, char **argv)
-{
-        if (argc != 2) {
-        std::cerr << "Uso: " << argv[0] << " <numero>" << std::endl;
-        return 1;
-        }
-    int n = std::stoi(argv[1]);
+int main(int argc, char **argv){
     
+    //verifica que el numero de argumentos sea el nombre y el argumento (osea 2), si no se cumple, muestra un mensaje de error
+    if (argc != 2){
+        std::cerr << "Uso: " << argv[0] << " <numero>" << "\n";
+        return 1; //aca el 1 actua como false solo que sin ser booleano (no hay funcion bool)
+    }
+    
+    //convierte el argumento a un entero (como visto en clase)
+    int n = std::stoi(argv[1]);
+
+    //inicia la variable suma en 0 para acumular los valores
     long sum = 0;
-    for (long ii = 2; ii <= n; ++ii)
-    {
-        if (isprime(ii) == true)
-        {
+
+    //inicia el bucle desde 2 hasta n, incrementa
+    for (long ii = 2; ii <= n; ++ii){
+        //verifica que se cumpla el ser primo con la funcion
+        if (isprime(ii)){
             sum += ii;
         }
     }
+
+    //imprime el valor final de la suma
     std::cout << "La suma es = " << sum << "\n";
     return 0;
-}
+ }
+
+
+//NOTA: Para que funcione (ya que tiene que leer los parametros desde la linea de comando), se debe escribir ./a.out y el numero
+//Ejemplo: ./a.out 5, deberia retornar "La suma es= 10"
