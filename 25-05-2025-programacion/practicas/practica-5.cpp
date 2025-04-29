b86f1b9fbd252ac35aa80c702ef869cf5d8b1609
dfuentesn <dfuentesn@unal.edu.co>
2025-04-29 15:05:25 -0500

    entrada y salida

---
+++ 25-05-2025-programacion/practicas/practica-5.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+//Operadores de entrada y salida
+int main (){
+    int k, n, suma;
+    std::cout << "Ingrese un numero" << "\n";
+    std::cin >> k;
+    std::cout << "Ingrese otro numero" << "\n";
+    std::cin >> n;
+
+    suma = k + n;
+      std::cout << "La suma es =" << suma << "\n";
+  
+    return 0;
+}
