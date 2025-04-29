9b9e243a90b6026a4f3b10698e7c287fa2d04bae
dfuentesn <dfuentesn@unal.edu.co>
2025-04-29 14:44:56 -0500

    Operadores basicos

---
+++ 25-05-2025-programacion/practicas/practica-2.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+//Operadores basicos
+int main (){
+    int k = 7, n = 2; //si repetimos tipo de dato se puede separar con una coma
+        //en los modulos tenemos +, -, * y \. Suma, resta, multiplicacion y division
+       //para hallar el modulo %
+    
+    int suma;
+
+    suma = k + n;  //relacion que queremos traer
+
+    std::cout << "el resultado es =" << suma << "\n";
+
+    return 0;
+}
