d2d0f1025d29ee1cf96f0fbc61cf322c271000ca
dfuentesn <dfuentesn@unal.edu.co>
2025-04-29 21:56:12 -0500

    version final del mayor primo

--- workshops/programa1.cpp
+++ workshops/programa1.cpp
@@ -2,21 +2,21 @@
 #include <cmath>
 #include <string>
 
-//establecemos la funcion isprime para definit que es un primo
+//establecemos la funcion isprime para definir que es un primo
 bool isprime(long n){
 
-    // aca establecemos un limite inferior, todo n igual o menor a 1 no sera ni tendra primmos por ende su suma sera 0 
+    //acá establecemos un límite inferior, todo n igual o menor a 1 no será ni tendra primmos, por ende su suma será 0 
     if (n <= 1){
         return false;
     } 
 
-    // aca decimos que 2 es el unico par primo porque retorna como verdadero
+    //acá decimos que 2 es el único par primo porque retorna como verdadero
     bool flag = true;
     if (n == 2){
         return true;
     } 
 
-    //iteramos desde 2 y verificamos que todos los demas n sean divisibles por la raiz del numero mas 1 (el 1 es un precaucion, como vizto en clase)
+    //iteramos desde 2 y verificamos que todos los demás n sean divisibles por la raíz del número más 1 (el 1 es un precaución, como visto en clase)
     for (long ii = 2; ii <= std::sqrt(n) + 1; ii++){
         if (n % ii == 0){
             flag = false;
@@ -28,10 +28,10 @@ bool isprime(long n){
 
 int main(int argc, char **argv){
 
-    //verifica que el numero de argumentos sea el nombre y el argumento (osea 2), si no se cumple, muestra un mensaje de error
+    //verifica que el número de argumentos sea el nombre y el argumento (osea 2), si no se cumple, muestra un mensaje de error
     if (argc != 2){
         std::cerr << "Uso: " << argv[0] << " <numero>" << "\n";
-        return 1; //aca el 1 actua como false solo que sin ser booleano (no hay funcion bool)
+        return 1; //acá el 1 actua como false solo que sin ser booleano (no hay funcion bool)
     }
     
     //convierte el argumento a un entero (como visto en clase)
@@ -40,9 +40,9 @@ int main(int argc, char **argv){
     //inicia la variable suma en 0 para acumular los valores
     long sum = 0;
 
-    //inicia el bucle desde 2 hasta n, incrementa
+    //inicia el bucle desde 2 hasta n; incrementa
     for (long ii = 2; ii <= n; ++ii){
-        //verifica que se cumpla el ser primo con la funcion
+        //verifica que se cumpla el ser primo con la función:
         if (isprime(ii)){
             sum += ii;
         }
@@ -54,5 +54,5 @@ int main(int argc, char **argv){
  }
 
 
-//NOTA: Para que funcione (ya que tiene que leer los parametros desde la linea de comando), se debe escribir ./a.out y el numero
-//Ejemplo: ./a.out 5, deberia retornar "La suma es= 10"
+//NOTA: Para que funcione (ya que tiene que leer los parámetros desde la línea de comando), se debe escribir ./a.out y el número
+//Ejemplo: ./a.out 5. Debería retornar "La suma es= 10"
