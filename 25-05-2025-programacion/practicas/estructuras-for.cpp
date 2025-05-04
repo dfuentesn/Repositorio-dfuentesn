1d742e09867539c9c7a8b4e89f53be330ad5dc2c
dfuentesn <dfuentesn@unal.edu.co>
2025-05-04 14:08:32 -0500

    como funciona el for

--- 25-05-2025-programacion/practicas/estructuras-for.cpp
+++ 25-05-2025-programacion/practicas/estructuras-for.cpp
@@ -2,6 +2,7 @@
 
 int main(){
     //numeros del 1 a 8
+    // primero es (type variable variable donde incia; hasta donde va; incrementa o no)
     for (int i = 1; i <= 8; i++){
       std::cout << i << " "; //si cabio el i por una frase o algo asi, imprimira la frase esa cantidad de veces
   }
