198c4027d0e119b9ba56de98659055216b612d22
dfuentesn <dfuentesn@unal.edu.co>
2025-05-04 14:39:55 -0500

    cadena de impares hasta 40

---
+++ 25-05-2025-programacion/practicas/impares-hasta-el-40.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+
+    std::cout << "La cadena es = ";
+
+     for (int k = 1; k <= 40; k = k + 2){
+
+      std::cout << k << " ";
+     }
+
+    std::cout << "\n";
+
+    return 0;
+}
