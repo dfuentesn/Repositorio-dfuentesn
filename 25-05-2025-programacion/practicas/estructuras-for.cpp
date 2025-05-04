161d638b37cd26e97585a26037a33ec298b6ccdb
dfuentesn <dfuentesn@unal.edu.co>
2025-05-04 14:07:45 -0500

    como funciona el for

---
+++ 25-05-2025-programacion/practicas/estructuras-for.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(){
+    //numeros del 1 a 8
+    for (int i = 1; i <= 8; i++){
+      std::cout << i << " "; //si cabio el i por una frase o algo asi, imprimira la frase esa cantidad de veces
+  }
+  std::cout << "\n";
+
+    return 0;
+}
